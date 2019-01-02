8ced833333edbb074415e94d46dafeed93df77f2
Stephen von Takach <steve@acaprojects.com>
2019-01-02 13:45:32 +1100

    add redis storage for persisting status values
    implements a very simple ORM, mapping a Crystal Hash to a Redis Hash object.

---
+++ spec/storage_spec.cr
@@ -0,0 +1,51 @@
+require "./helper"
+
+describe EngineDriver::Storage do
+  it "should perform basic storage operations" do
+    store = EngineDriver::Storage.new("test-123")
+    store.size.should eq(0)
+    store[:test] = "null"
+    store.size.should eq(1)
+    store[:test].should eq("null")
+    store.delete(:test).should eq("null")
+    store.size.should eq(0)
+
+    store[:test] = "null"
+    store.size.should eq(1)
+
+    store[:test] = nil
+    store.size.should eq(0)
+
+    store[:what]?.should eq(nil)
+  end
+
+  it "should return keys and values" do
+    store = EngineDriver::Storage.new("test-123")
+    store[:test] = "null"
+    store[:other] = "1234"
+    store.size.should eq(2)
+
+    store.keys.should eq(["test", "other"])
+    store.values.should eq(["null", "1234"])
+
+    store.empty?.should eq(false)
+
+    store.clear
+    store.size.should eq(0)
+
+    store.empty?.should eq(true)
+  end
+
+  it "should generate a crystal hash" do
+    store = EngineDriver::Storage.new("test-123")
+    store[:test] = "null"
+    store[:other] = "1234"
+    store.size.should eq(2)
+    store.to_h.should eq({
+      "test" => "null",
+      "other" => "1234"
+    })
+    store.clear
+    store.size.should eq(0)
+  end
+end
---
+++ src/engine-driver/storage.cr
@@ -0,0 +1,85 @@
+require "redis"
+
+class EngineDriver::Storage < Hash(String, String)
+  @@instance : Redis::PooledClient?
+
+  def initialize(@module_id : String, prefix = "status")
+    super()
+    @@instance ||= Redis::PooledClient.new
+    @redis = @@instance.not_nil!
+    @hash_key = "#{prefix}\x02#{@module_id}"
+  end
+
+  @redis : Redis::PooledClient
+  getter :hash_key, :redis
+
+  def []=(status_name, json_value)
+    if json_value.nil?
+      delete(status_name)
+    else
+      status_name = status_name.to_s
+      key = hash_key
+      @redis.pipelined do |pipeline|
+        pipeline.hset(key, status_name, json_value)
+        pipeline.publish("#{key}\x02#{status_name}", json_value)
+      end
+    end
+    json_value
+  end
+
+  def fetch(key)
+    key = key.to_s
+    entry = @redis.hget(hash_key, key)
+    entry ? entry.to_s : yield key
+  end
+
+  def delete(key)
+    key = key.to_s
+    value = self[key]?
+    if value
+      hkey = hash_key
+      @redis.pipelined do |pipeline|
+        pipeline.hdel(hkey, key)
+        pipeline.publish("#{hkey}\x02#{key}", "null")
+      end
+      return value.to_s
+    end
+    yield key
+  end
+
+  def keys
+    @redis.hkeys(hash_key).map &.to_s
+  end
+
+  def values
+    @redis.hvals(hash_key).map &.to_s
+  end
+
+  def size
+    @redis.hlen(hash_key)
+  end
+
+  def empty?
+    size == 0
+  end
+
+  def to_h
+    hash = {} of String => String
+    @redis.hgetall(hash_key).each_slice(2) do |slice|
+      hash[slice[0].to_s] = slice[1].to_s
+    end
+    hash
+  end
+
+  def clear
+    hkey = hash_key
+    keys = @redis.hkeys(hkey)
+    @redis.pipelined do |pipeline|
+      keys.each do |key|
+        pipeline.hdel(hkey, key)
+        pipeline.publish("#{hkey}\x02#{key}", "null")
+      end
+    end
+    self
+  end
+end
