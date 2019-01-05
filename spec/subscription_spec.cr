bf4ec17fa85c5a487ad7c133748a3fa434f43a69
Stephen von Takach <steve@acaprojects.com>
2019-01-06 00:07:51 +1100

    complete initial subscription spec

--- spec/subscription_spec.cr
+++ spec/subscription_spec.cr
@@ -2,8 +2,23 @@ require "./helper"
 
 describe EngineDriver::Subscriptions do
   it "should subscribe to a channel" do
+    in_callback = false
+    sub_passed = nil
+    message_passed = nil
+    channel = Channel(Nil).new
+
     subs = EngineDriver::Subscriptions.new
-    subs.channel "test" do |sub, message|
+    subscription = subs.channel "test" do |sub, message|
+      sub_passed = sub
+      message_passed = message
+      in_callback = true
+      channel.close
     end
+    EngineDriver::Storage.redis_pool.publish("engine\x02test", "whatwhat")
+    channel.receive?
+
+    in_callback.should eq(true)
+    message_passed.should eq("whatwhat")
+    sub_passed.should eq(subscription)
   end
 end
