2946ffb747850bc66ebf5f37ef2c495e2d44b442
Stephen von Takach <steve@acaprojects.com>
2019-07-25 22:13:49 +1000

    (interface:moveable) add comments on status

--- src/engine-driver/interface/moveable.cr
+++ src/engine-driver/interface/moveable.cr
@@ -12,5 +12,9 @@ module EngineDriver::Interface::Moveable
     Out
   end
 
+  # Expects status of the movable object to be defined as
+  # position{index} = Open / Close / Up / Down etc
+  # i.e. self[:position0] = MoveablePosition::Open
+  # (optional) self[moving0] = true / false
   abstract def move(position : MoveablePosition, index : Int32 | String = 0)
 end
