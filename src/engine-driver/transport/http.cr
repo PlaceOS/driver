7e701ed3da0109349d184ff349b558ff3d0ed980
Caspian Baska <caspianbaska@gmail.com>
2019-12-10 17:51:19 +1100

    feat(transport/http): {HTTP verb}_request macros

--- src/engine-driver/transport/http.cr
+++ src/engine-driver/transport/http.cr
@@ -2,6 +2,68 @@ require "http"
 require "./http_proxy"
 
 class ACAEngine::Driver
+  {% for method in %w(get post put head delete patch options) %}
+    # Executes a {{method.id.upcase}} request on the client connection.
+    #
+    # The response status will be automatically checked and an error raised if unsuccessful
+    #
+    # Macro expansion allows this to obtain context from a surround method and
+    # use method arguments to build an appropriate request structure.
+    #
+    # Us *as* to specify a JSON parse-able model that the response should be
+    # piped into. If unspecified a `JSON::Any` will be returned.
+    private macro {{method.id}}_request(path, params = {} of String => String?, headers = HTTP::Headers.new, body = nil, secure = false, concurrent = false, as model = nil)
+      {% verbatim do %}
+        path = {{path}}
+        headers = {{headers}}
+
+        # Build a body (if required)
+        {% if body.is_a? NamedTupleLiteral %}
+          headers ||= HTTP::Headers.new
+          headers["Content-Type"] = "application/json"
+          body = JSON.build do |json|
+            json.object do
+              {% for key, value in body %}
+                json.field {{key.stringify}}, {{value}}
+              {% end %}
+            end
+          end
+        {% else %}
+          body = {{body}}
+        {% end %}
+
+        concurrent = {{concurrent}}
+        secure = {{secure}}
+      {% end %}
+
+      # Exec the request
+      response = transport.http(
+        method: {{method.upcase.stringify.id}},
+        path: path,
+        body: body,
+        headers: headers,
+        concurrent: concurrent,
+        secure: secure,
+      )
+      unless response.success?
+        logger.debug { response.body }
+        raise "unexpected response #{response.status_code}\n#{response.body}"
+      end
+      # Parse the response
+      {% verbatim do %}
+        {% if model %}
+          {{model}}.from_json response.body
+        {% else %}
+          if response.body.empty?
+            JSON::Any.new nil
+          else
+            JSON.parse response.body
+          end
+        {% end %}
+      {% end %}
+    end
+  {% end %}
+
   # Implement the HTTP helpers
   protected def http(method, path, body : ::HTTP::Client::BodyType = nil,
                      params : Hash(String, String?) = {} of String => String?,
