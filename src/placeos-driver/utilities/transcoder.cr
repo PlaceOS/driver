6fffd3f3d7547511f7a586f21289f6ca3434269a
Philip Kheav <philip.kheav@hotmail.com>
2020-06-25 12:56:30 +0100

    fix(transcoder): add types to params

--- src/placeos-driver/utilities/transcoder.cr
+++ src/placeos-driver/utilities/transcoder.cr
@@ -1,13 +1,13 @@
 module PlaceOS::Driver::Utilities::Transcoder
   # Converts a hex encoded string into bytes
-  def hex_to_bytes(string)
+  def hex_to_bytes(string : String)
     string = string.gsub(/(0x|[^0-9A-Fa-f])*/, "")
     string = "0#{string}" if string.size % 2 > 0
     string.hexbytes
   end
 
   # Converts a byte array into bytes
-  def array_to_bytes(array)
+  def array_to_bytes(array : Array(UInt8)
     bytes = Bytes.new(array.size)
     array.each_with_index do |byte, index|
       bytes[index] = 0_u8 | byte
