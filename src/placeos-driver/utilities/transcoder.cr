4de30c1561a78b639dfe6b87b3faa2fb6dd14e54
Philip Kheav <z3463007@zmail.unsw.edu.au>
2020-07-07 14:28:31 +0100

    fix(transcoder): add types to params (#32)
    * fix(transcoder): add types to params
    
    * fix(transcoder): correct typo and allow Int32
    
    * feat(transcoder): allow all Integer sizes
    
    in array to bytes
    
    * Revert "feat(transcoder): allow all Integer sizes"
    
    This reverts commit 71d614b155d3bccc895ca0fa85581288f6926486.
    
    * fix(transcoder): make methods protected
    
    Co-authored-by: Stephen von Takach <steve@advancedcontrol.com.au>
    Co-authored-by: Stephen von Takach <steve@acaprojects.com>

--- src/placeos-driver/utilities/transcoder.cr
+++ src/placeos-driver/utilities/transcoder.cr
@@ -1,13 +1,13 @@
 module PlaceOS::Driver::Utilities::Transcoder
   # Converts a hex encoded string into bytes
-  def hex_to_bytes(string)
+  protected def hex_to_bytes(string)
     string = string.gsub(/(0x|[^0-9A-Fa-f])*/, "")
     string = "0#{string}" if string.size % 2 > 0
     string.hexbytes
   end
 
   # Converts a byte array into bytes
-  def array_to_bytes(array)
+  protected def array_to_bytes(array)
     bytes = Bytes.new(array.size)
     array.each_with_index do |byte, index|
       bytes[index] = 0_u8 | byte
