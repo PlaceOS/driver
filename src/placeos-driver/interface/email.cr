fb57ad1785c7daa1e1e04f139649e7f81f17af39
Caspian Baska <caspianbaska@gmail.com>
2020-08-04 23:24:46 +1000

    fix(interface:email): correct abstract signature

--- src/placeos-driver/interface/email.cr
+++ src/placeos-driver/interface/email.cr
@@ -7,13 +7,14 @@ module PlaceOS::Driver::Interface::Email
   alias ResourceAttachment = NamedTuple(file_name: String, content: String, content_id: String)
 
   abstract def send_email(
-    to : Array(String),
     subject : String,
-    message_html : String = nil,
-    message_plaintext : String = nil,
-    attachments : Array(Attachment)? = nil,
-    resource_attachments : Array(ResourceAttachment)? = nil,
-    cc : Array(String)? = nil,
-    bcc : Array(String)? = nil
+    to : String | Array(String),
+    from : String | Array(String) | Nil = nil,
+    message_html : String = "",
+    message_plaintext : String = "",
+    attachments : Array(Attachment) = [] of Attachment,
+    resource_attachments : Array(ResourceAttachment) = [] of ResourceAttachment,
+    cc : String | Array(String) = [] of String,
+    bcc : String | Array(String) = [] of String
   )
 end
