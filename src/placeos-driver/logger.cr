f1aa7ee2e3da32cce20f3b6414bfd65833d58103
Stephen von Takach <steve@vontaka.ch>
2020-09-18 00:00:48 +1000

    feat(logger): improve debug logs
    include backtraces

--- src/placeos-driver/logger.cr
+++ src/placeos-driver/logger.cr
@@ -33,7 +33,12 @@ class PlaceOS::Driver
     end
 
     def write(entry : ::Log::Entry)
-      protocol.request entry.source, "debug", [entry.severity, entry.message]
+      if exception = entry.exception
+        message = "#{entry.message}\n#{exception.inspect_with_backtrace}"
+        protocol.request entry.source, "debug", [entry.severity, message]
+      else
+        protocol.request entry.source, "debug", [entry.severity, entry.message]
+      end
     end
   end
 
