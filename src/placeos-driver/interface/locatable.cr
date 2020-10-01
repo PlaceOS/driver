23c47d92abc778500a3e4fef9632564ba1bf70fd
Stephen von Takach <steve@vontaka.ch>
2020-10-01 17:12:25 +1000

    fix(interface locatable): allow either or both email and username

--- src/placeos-driver/interface/locatable.cr
+++ src/placeos-driver/interface/locatable.cr
@@ -1,5 +1,5 @@
 module PlaceOS::Driver::Interface; end
 
 module PlaceOS::Driver::Interface::Locatable
-  abstract def locate_user(identifier : String)
+  abstract def locate_user(email : String? = nil, username : String? = nil)
 end
