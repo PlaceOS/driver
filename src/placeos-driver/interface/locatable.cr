800102e531927ec2970b1e001b7a0b194b0751aa
Stephen von Takach <steve@vontaka.ch>
2020-10-20 13:00:13 +1100

    fix(interface locatable): minor tweak to interface
    check_ownership_of mac might return nil

--- src/placeos-driver/interface/locatable.cr
+++ src/placeos-driver/interface/locatable.cr
@@ -8,12 +8,14 @@ module PlaceOS::Driver::Interface::Locatable
   # lowercase with no seperation characters abcdeffd1234 etc
   abstract def macs_assigned_to(email : String? = nil, username : String? = nil) : Array(String)
 
-  # return `nil` or `{"location": "wireless", "assigned_to": "bob123", "mac_address": "abcd"}`
-  abstract def check_ownership_of(mac_address : String) : NamedTuple(
+  alias OwnershipMAC = NamedTuple(
     location: String,
     assigned_to: String,
     mac_address: String,
   )
+
+  # return `nil` or `{"location": "wireless", "assigned_to": "bob123", "mac_address": "abcd"}`
+  abstract def check_ownership_of(mac_address : String) : OwnershipMAC?
 
   # array of devices and their x, y coordinates
   abstract def device_locations(zone_id : String, location : String? = nil)
