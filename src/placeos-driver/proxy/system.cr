5e5630de38eb4509cef9971e3214fbfff78cd3c8
Stephen von Takach <steve@vontaka.ch>
2021-01-07 12:36:46 +1100

    fix(system proxy): DRY up code

--- src/placeos-driver/proxy/system.cr
+++ src/placeos-driver/proxy/system.cr
@@ -58,15 +58,7 @@ class PlaceOS::Driver::Proxy::System
     index = index.to_i
 
     module_id = @system["#{module_name}/#{index}"]?
-    metadata = RedisStorage.get("interface/#{module_id}") if module_id
-    metadata = if module_id && metadata
-                 DriverModel::Metadata.from_json metadata
-               else
-                 # return a hollow proxy - we don't want to error
-                 # code can execute against a non-existance driver
-                 DriverModel::Metadata.new
-               end
-
+    metadata = get_metadata(module_id)
     module_id ||= "driver index unavailable"
 
     Proxy::Driver.new(@reply_id, module_name, index, module_id, self, metadata)
@@ -83,19 +75,23 @@ class PlaceOS::Driver::Proxy::System
 
       if mod_name == module_name
         module_id = @system[key]
-        metadata = RedisStorage.get("interface/#{module_id}")
-        metadata = if module_id && metadata
-                     DriverModel::Metadata.from_json metadata
-                   else
-                     # return a hollow proxy - we don't want to error
-                     # code can execute against a non-existance driver
-                     DriverModel::Metadata.new
-                   end
+        metadata = get_metadata(module_id)
         drivers << Proxy::Driver.new(@reply_id, module_name, index.to_i, module_id, self, metadata)
       end
     end
 
     PlaceOS::Driver::Proxy::Drivers.new(drivers)
+  end
+
+  private def get_metadata(module_id : String?) : DriverModel::Metadata
+    metadata_json = RedisStorage.get("interface/#{module_id}") if module_id.presence
+    if metadata_json && !metadata_json.empty?
+      DriverModel::Metadata.from_json metadata_json
+    else
+      # return a hollow proxy - we don't want to error
+      # code can execute against a non-existance driver
+      DriverModel::Metadata.new
+    end
   end
 
   # grabs all modules implementing(Powerable) for example
