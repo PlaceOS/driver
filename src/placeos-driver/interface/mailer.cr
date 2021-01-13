f52509d04e109d3a5768bea8ca00937ee11e13e4
Stephen von Takach <steve@vontaka.ch>
2021-01-13 16:05:04 +1100

    feat(interface mailer): add basic templating support

--- src/placeos-driver/interface/mailer.cr
+++ src/placeos-driver/interface/mailer.cr
@@ -17,4 +17,34 @@ module PlaceOS::Driver::Interface::Mailer
     bcc : String | Array(String) = [] of String,
     from : String | Array(String) | Nil = nil
   )
+
+  #                   event_name => notify_who => html => template
+  alias Templates = Hash(String, Hash(String, Hash(String, String)))
+  @templates : Templates = Templates.new
+
+  alias TemplateItems = Hash(String, String | Int64 | Float64 | Bool)
+
+  def send_template(
+    to : String | Array(String),
+    template : Tuple(String, String),
+    args : TemplateItems,
+    resource_attachments : Array(ResourceAttachment) = [] of ResourceAttachment,
+    attachments : Array(Attachment) = [] of Attachment,
+    cc : String | Array(String) = [] of String,
+    bcc : String | Array(String) = [] of String,
+    from : String | Array(String) | Nil = nil
+  )
+    template = @templates[template[0]][template[1]]
+
+    subject = build_template(template["subject"], args)
+    text = build_template(template["text"]?, args)
+    html = build_template(template["html"]?, args)
+
+    send_mail(to, subject, text || "", html || "", resource_attachments, attachments, cc, bcc, from)
+  end
+
+  def build_template(string : String?, args : TemplateItems)
+    args.each { |key, value| string = string.gsub("%{#{key}}", value) } if string
+    string
+  end
 end
