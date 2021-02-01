ec28213019dd53379c702b69a64978ac4a65f521
Stephen von Takach <steve@vontaka.ch>
2021-02-01 15:15:33 +1100

    fix: include a fix for Hash dup issues
    caused by generics inheritance

--- src/placeos-driver/storage.cr
+++ src/placeos-driver/storage.cr
@@ -7,4 +7,15 @@ abstract class PlaceOS::Driver::Storage < Hash(String, String)
   abstract def signal_status(status_name) : String?
 end
 
+# Fix for a Hash dup issues on crystal 0.36.0
+{% if compare_versions(Crystal::VERSION, "0.36.0") == 0 %}
+  class Hash(K, V)
+    def dup
+      hash = Hash(K, V).new
+      hash.initialize_dup(self)
+      hash
+    end
+  end
+{% end %}
+
 require "./storage/redis-storage"
