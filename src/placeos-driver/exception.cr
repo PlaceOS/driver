6c5e97e104e4942a980dd284a5ea424d4db4446a
Caspian Baska <caspianbaska@gmail.com>
2021-03-22 13:12:44 +1100

    style: more explicit getters

--- src/placeos-driver/exception.cr
+++ src/placeos-driver/exception.cr
@@ -1,9 +1,7 @@
 class PlaceOS::Driver::RemoteException < Exception
+  getter? backtrace
+
   def initialize(message, class_name, @backtrace = [] of String)
     @message = "#{message} (#{class_name})"
-  end
-
-  def backtrace?
-    @backtrace
   end
 end
