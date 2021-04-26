82d85d035aebd32cf249fe1eb5da2e4d322f4b88
Stephen von Takach <steve@vontaka.ch>
2021-04-27 09:27:43 +1000

    fix(interface electrical relay): changes requested from review
    sorry @kimburgess merged accidentally

--- src/placeos-driver/interface/electrical_relay.cr
+++ src/placeos-driver/interface/electrical_relay.cr
@@ -1,17 +1,18 @@
 abstract class PlaceOS::Driver; end
 
 module PlaceOS::Driver::Interface::ElectricalRelay
+  # `**options` here should be passed to the `task` to allow for different priorities
   abstract def relay(state : Bool, index : Int32 = 0, **options)
 
-  def pulse(period : Float64 = 1.0, index : Int32 = 0, times : Int32 = 1, initial_state = false)
+  def pulse(period : Int32 = 1000, index : Int32 = 0, times : Int32 = 1, initial_state = false)
     queue(name: "pulse_#{index}") do |task|
       # higher pririty so these commands run next once added to the queue
       priority = queue.priority + 25
-      period = pause_for.seconds
+      pause_for = period.milliseconds
 
       times.times do
-        relay(!initial_state, index, delay: period, priority: priority)
-        relay(initial_state, index, delay: period, priority: priority)
+        relay(!initial_state, index, delay: pause_for, priority: priority)
+        relay(initial_state, index, delay: pause_for, priority: priority)
       end
       task.success
     end
