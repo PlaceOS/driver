91799f85e43595ca7282f7bbf775a421795decf6
Caspian Baska <caspianbaska@gmail.com>
2021-05-03 16:37:07 +1000

    fix(interface/electrical_relay): add missing type to #pulse

--- src/placeos-driver/interface/electrical_relay.cr
+++ src/placeos-driver/interface/electrical_relay.cr
@@ -4,7 +4,7 @@ module PlaceOS::Driver::Interface::ElectricalRelay
   # `**options` here should be passed to the `task` to allow for different priorities
   abstract def relay(state : Bool, index : Int32 = 0, **options)
 
-  def pulse(period : Int32 = 1000, index : Int32 = 0, times : Int32 = 1, initial_state = false)
+  def pulse(period : Int32 = 1000, index : Int32 = 0, times : Int32 = 1, initial_state : Bool = false)
     queue(name: "pulse_#{index}") do |task|
       # higher pririty so these commands run next once added to the queue
       priority = queue.priority + 25
