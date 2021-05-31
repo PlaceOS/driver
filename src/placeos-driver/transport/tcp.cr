48dbc1559f0c23a95f41a9b7ea824059014c72ab
Stephen von Takach <steve@place.tech>
2021-05-31 13:28:56 +1000

    Update src/placeos-driver/transport/tcp.cr
    Co-authored-by: Caspian Baska <caspian@place.tech>

--- src/placeos-driver/transport/tcp.cr
+++ src/placeos-driver/transport/tcp.cr
@@ -107,11 +107,9 @@ class PlaceOS::Driver::TransportTCP < PlaceOS::Driver::Transport
       socket = @socket
       return self if socket.nil? || socket.closed?
 
-      if message.responds_to? :to_io
-        socket.write_bytes(message)
-      elsif message.responds_to? :to_slice
-        data = message.to_slice
-        socket.write data
+      case message
+      when .responds_to? :to_io    then socket.write_bytes(message)
+      when .responds_to? :to_slice then socket.write message.to_slice
       else
         socket << message
       end
