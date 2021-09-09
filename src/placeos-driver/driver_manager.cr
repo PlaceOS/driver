72c1048dda91c26d92ba74e15dc4fd8e54f5554e
Stephen von Takach <steve@vontaka.ch>
2021-09-09 16:07:31 +1000

    only output status if true
    to reduce noise

--- src/placeos-driver/driver_manager.cr
+++ src/placeos-driver/driver_manager.cr
@@ -218,6 +218,6 @@ class PlaceOS::Driver::DriverManager
   end
 
   private def check_proxy_usage(driver)
-    driver[:using_proxy] = @transport.using_proxy.nil? ? nil : @transport.using_proxy
+    driver[:using_proxy] = @transport.using_proxy ? true : nil
   end
 end
