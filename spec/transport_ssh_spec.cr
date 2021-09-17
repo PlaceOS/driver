728b7a6a3805d013acdafe8e4ab98099f8430b36
Stephen von Takach <steve@place.tech>
2021-09-17 14:55:19 +1000

    chore(specs): re-enable ssh spec (#121)
    * chore(specs): re-enable ssh spec
    * improve reliability of spec
    * fix ssh service, no ports exposed and spec to match
    * change how response is processed

--- spec/transport_ssh_spec.cr
+++ spec/transport_ssh_spec.cr
@@ -2,11 +2,10 @@ require "./helper"
 
 describe PlaceOS::Driver::TransportSSH do
   it "should work with a received function" do
-    pending!("spec is hanging in CI") if ENV["CI"]?
+    pending!("spec only available in CI") unless ENV["CI"]?
 
     queue = Helper.queue
-    count = 0
-    transport = PlaceOS::Driver::TransportSSH.new(queue, "localhost", 2222, ::PlaceOS::Driver::Settings.new(%({
+    transport = PlaceOS::Driver::TransportSSH.new(queue, "sshtest", 22, ::PlaceOS::Driver::Settings.new(%({
       "ssh": {
         "username": "root",
         "password": "somepassword"
@@ -14,15 +13,15 @@ describe PlaceOS::Driver::TransportSSH do
     }))) do |data, task|
       # This would usually call: driver.received(data, task)
       response = IO::Memory.new(data).to_s
-      count += 1
-      task.try &.success(response) if count >= 3 && !response.includes?("root@")
+      puts "ssh-sent: #{response}"
+      task.try &.success(response) if response.includes?("USER")
     end
 
     transport.connect
     queue.online.should eq(true)
 
-    task = queue.add { transport.send("ls /\n") }.response_required!
-    task.get.payload.includes?("bin").should eq(true)
+    task = queue.add { transport.send("ps aux\n") }.response_required!
+    task.get.payload.includes?("USER").should eq(true)
 
     # Close the connection
     transport.terminate
