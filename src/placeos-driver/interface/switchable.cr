c9affb87896d6bf1885bdc11bb6ebe817d4a27f2
Stephen von Takach <steve@vontaka.ch>
2021-10-18 17:00:13 +1100

    feat(interface switchable): simplify interface

--- src/placeos-driver/interface/switchable.cr
+++ src/placeos-driver/interface/switchable.cr
@@ -5,8 +5,8 @@ abstract class PlaceOS::Driver
     abstract def switch_to(input : Input)
   end
 
-  module PlaceOS::Driver::Interface::Switchable(Input, Output)
-    include PlaceOS::Driver::Interface::InputSelection(Input)
+  module Interface::Switchable(Input, Output)
+    include Interface::InputSelection(Input)
 
     enum SwitchLayer
       All
@@ -16,14 +16,6 @@ abstract class PlaceOS::Driver
       Data2
     end
 
-    macro included
-      # { layer => { input => [output1, output2] } }
-      alias SelectiveSwitch = Hash(String, Hash(Input, Array(Output)))
-
-      # {input => [output1, output2]}
-      alias FullSwitch = Hash(Input, Array(Output))
-    end
-
-    abstract def switch(map : Hash(Input, Array(Output)) | Hash(String, Hash(Input, Array(Output))))
+    abstract def switch(map : Hash(Input, Array(Output)), layer : SwitchLayer? = nil)
   end
 end
