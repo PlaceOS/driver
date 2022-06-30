76ef3bee13c162977b88077d18ed976db4e7c9c3
Stephen von Takach <steve@place.technology>
2022-06-30 12:17:11 +1000

    fix: re-establish the signal registration once executed

--- src/placeos-driver/stats.cr
+++ src/placeos-driver/stats.cr
@@ -8,9 +8,12 @@ module PlaceOS::Driver::Stats
     one_mib = 1048576
     Log.warn { "\n\nMemory Usage:\n - Free: #{(stats.free_bytes / one_mib).round(1)}MiB\n - Heap: #{(stats.heap_size / one_mib).round(1)}MiB\n - Total: #{(stats.total_bytes / one_mib).round(1)}MiB\n - Unmapped #{(stats.unmapped_bytes / one_mib).round(1)}MiB\n\n" }
   end
-end
 
-Signal::USR1.trap do |signal|
-  spawn { PlaceOS::Driver::Stats.dump_stats }
-  signal.ignore
+  def self.setup_signal
+    Signal::USR2.trap do |signal|
+      spawn { PlaceOS::Driver::Stats.dump_stats }
+      signal.ignore
+      setup_signal
+    end
+  end
 end
