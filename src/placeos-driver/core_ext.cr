b471465b7b396538cd6c9a03687a9cbd3b57af35
Stephen von Takach <steve@place.technology>
2022-09-20 14:43:50 +1000

    fix(core_ext): add monkey patch for DWARF 5 support
    fixes statically built apps

--- src/placeos-driver/core_ext.cr
+++ src/placeos-driver/core_ext.cr
@@ -19,3 +19,94 @@ class Object
     fallback
   end
 end
+
+# NOTE:: fixes issues with Crystal 1.5.0
+# TODO:: remove once this is mainline: https://github.com/crystal-lang/crystal/pull/12497
+
+require "crystal/dwarf/info"
+
+# debugging dwarf file issues
+struct Crystal::DWARF::Info
+  private def read_attribute_value(form, attr)
+    case form
+    when FORM::Addr
+      case address_size
+      when 4 then @io.read_bytes(UInt32)
+      when 8 then @io.read_bytes(UInt64)
+      else        raise "Invalid address size: #{address_size}"
+      end
+    when FORM::Block1
+      len = @io.read_byte.not_nil!
+      @io.read_fully(bytes = Bytes.new(len.to_i))
+      bytes
+    when FORM::Block2
+      len = @io.read_bytes(UInt16)
+      @io.read_fully(bytes = Bytes.new(len.to_i))
+      bytes
+    when FORM::Block4
+      len = @io.read_bytes(UInt32)
+      @io.read_fully(bytes = Bytes.new(len.to_i64))
+      bytes
+    when FORM::Block
+      len = DWARF.read_unsigned_leb128(@io)
+      @io.read_fully(bytes = Bytes.new(len))
+      bytes
+    when FORM::Data1
+      @io.read_byte.not_nil!
+    when FORM::Data2
+      @io.read_bytes(UInt16)
+    when FORM::Data4
+      @io.read_bytes(UInt32)
+    when FORM::Data8
+      @io.read_bytes(UInt64)
+    when FORM::Data16
+      @io.read_bytes(UInt64)
+      @io.read_bytes(UInt64)
+    when FORM::Sdata
+      DWARF.read_signed_leb128(@io)
+    when FORM::Udata
+      DWARF.read_unsigned_leb128(@io)
+    when FORM::ImplicitConst
+      attr.value
+    when FORM::Exprloc
+      len = DWARF.read_unsigned_leb128(@io)
+      @io.read_fully(bytes = Bytes.new(len))
+      bytes
+    when FORM::Flag
+      @io.read_byte == 1
+    when FORM::FlagPresent
+      true
+    when FORM::SecOffset
+      read_ulong
+    when FORM::Ref1
+      @ref_offset + @io.read_byte.not_nil!.to_u64
+    when FORM::Ref2
+      @ref_offset + @io.read_bytes(UInt16).to_u64
+    when FORM::Ref4
+      @ref_offset + @io.read_bytes(UInt32).to_u64
+    when FORM::Ref8
+      @ref_offset + @io.read_bytes(UInt64).to_u64
+    when FORM::RefUdata
+      @ref_offset + DWARF.read_unsigned_leb128(@io)
+    when FORM::RefAddr
+      read_ulong
+    when FORM::RefSig8
+      @io.read_bytes(UInt64)
+    when FORM::String
+      @io.gets('\0', chomp: true).to_s
+    when FORM::Strp, FORM::LineStrp
+      # HACK: A call to read_ulong is failing with an .ud2 / Illegal instruction: 4 error
+      #       Calling with @[AlwaysInline] makes no difference.
+      if @dwarf64
+        @io.read_bytes(UInt64)
+      else
+        @io.read_bytes(UInt32)
+      end
+    when FORM::Indirect
+      form = FORM.new(DWARF.read_unsigned_leb128(@io))
+      read_attribute_value(form, attr)
+    else
+      raise "Unknown DW_FORM_#{form.to_s.underscore}"
+    end
+  end
+end
