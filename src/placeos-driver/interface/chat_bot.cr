2467d9078f55b8e0344d2a5ccca4fda5e6300708
Stephen von Takach <steve@place.technology>
2022-11-28 15:15:51 +1100

    fix(interface/chat_bot): improve chat bot interface

--- src/placeos-driver/interface/chat_bot.cr
+++ src/placeos-driver/interface/chat_bot.cr
@@ -1,17 +1,42 @@
+require "json"
+
 abstract class PlaceOS::Driver
   module Interface::ChatBot
-    # drivers are expected to emit door state events on
-    # channel security/event/door
-    class Message
+    # NOTE:: expects messages to broadcast to a channel, such as: chat/<bot_name>/<org_id>/message
+
+    struct Id
       include JSON::Serializable
 
+      # something used to identify the message
+      property message_id : String
+
       # The room ID of the message.
-      @[JSON::Field(key: "roomId")]
-      property room_id : String
+      property room_id : String?
+
+      # The user who sent the message
+      property user_id : String?
+    end
+
+    struct Message
+      include JSON::Serializable
+
+      property id : Id
 
       # The message, in plain text.
-      @[JSON::Field(key: "text")]
       property text : String
     end
+
+    struct Attachment
+      include JSON::Serializable
+
+      property name : String
+
+      # Base64 encoded binary contents
+      property payload : String
+    end
+
+    abstract def notify_typing(id : Id)
+
+    abstract def reply(id : Id, response : String, url : String? = nil, attachment : Attachment? = nil)
   end
 end
