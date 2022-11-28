06c7629494aa4022fa9e632d8dbd24c3db485530
Stephen von Takach <steve@place.technology>
2022-11-28 16:34:09 +1100

    fix(interface/chat_bot): improve chat bot interface (#160)

--- src/placeos-driver/interface/chat_bot.cr
+++ src/placeos-driver/interface/chat_bot.cr
@@ -1,17 +1,56 @@
+require "json"
+
 abstract class PlaceOS::Driver
   module Interface::ChatBot
-    # drivers are expected to emit door state events on
-    # channel security/event/door
-    class Message
+    # NOTE:: expects messages to broadcast to a channel, such as: chat/<bot_name>/<org_id>/message
+
+    struct Id
       include JSON::Serializable
 
+      def initialize(@message_id, @room_id = nil, @user_id = nil, @org_id = nil)
+      end
+
+      # something used to identify the message
+      property message_id : String
+
       # The room ID of the message.
-      @[JSON::Field(key: "roomId")]
-      property room_id : String
+      property room_id : String?
+
+      # The user who sent the message
+      property user_id : String?
+
+      # The org sending the message.
+      property org_id : String?
+    end
+
+    struct Message
+      include JSON::Serializable
+
+      def initialize(@id, @text)
+      end
+
+      property id : Id
 
       # The message, in plain text.
-      @[JSON::Field(key: "text")]
       property text : String
     end
+
+    struct Attachment
+      include JSON::Serializable
+
+      def initialize(@name, @payload)
+      end
+
+      property name : String
+
+      # Base64 encoded binary contents
+      property payload : String
+    end
+
+    # allows a bot responder to indicate it is replying to a message
+    abstract def notify_typing(id : Id)
+
+    # interface used to reply to a message
+    abstract def reply(id : Id, response : String, url : String? = nil, attachment : Attachment? = nil)
   end
 end
