ec756a91eb7463a61c5df85034ae682def7fae87
Stephen von Takach <steve@place.technology>
2024-06-20 16:34:19 +1000

    feat(interface/desk_control): add desk control interface

---
+++ src/placeos-driver/interface/desk_control.cr
@@ -0,0 +1,21 @@
+abstract class PlaceOS::Driver
+  # Common desk control interface
+  module Interface::DeskControl
+    # desk height is in mm
+    abstract def set_desk_height(
+      desk_key : String,
+      desk_height : Int32? = nil
+    )
+
+    # return nil on unknown height
+    abstract def get_desk_height(desk_key : String) : Int32?
+
+    # desk_power on / off / nil == auto
+    abstract def set_desk_power(
+      desk_key : String,
+      desk_power : Bool?
+    )
+
+    abstract def get_desk_power(desk_key : String) : Bool?
+  end
+end
