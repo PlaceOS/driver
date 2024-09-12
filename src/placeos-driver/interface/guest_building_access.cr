0d5a935736ed4b72a19d09f32c2f98ed393c4716
Stephen von Takach <steve@place.technology>
2024-09-12 22:05:23 +1000

    fix(interface/guest_building_access): details class resolution

--- src/placeos-driver/interface/guest_building_access.cr
+++ src/placeos-driver/interface/guest_building_access.cr
@@ -12,9 +12,9 @@ abstract class PlaceOS::Driver
 
     macro included
       {% begin %}
-        alias SubKlassAccessDetails = {{ parse_type("::AccessDetails").resolve.subclasses.first }}
+        alias SubKlassAccessDetails = {{ parse_type("::PlaceOS::Driver::Interface::GuestBuildingAccess::AccessDetails").resolve.subclasses.first }}
       {% end %}
-      
+
       # revoke access to a building
       def revoke_guest_access(details : SubKlassAccessDetails) : Nil
         revoke_access details
