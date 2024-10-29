eea9610f968c2ce2afd703da504a19a7a5ceb0e6
Mia Bennett <mia@chillfox.com>
2024-10-29 09:50:10 +0930

    refactor(MailerTemplates): change #template_fields return time to be compatible with json

--- src/placeos-driver/interface/mailer_templates.cr
+++ src/placeos-driver/interface/mailer_templates.cr
@@ -5,33 +5,37 @@ module PlaceOS::Driver::Interface; end
 
 # Common Email templates interface
 module PlaceOS::Driver::Interface::MailerTemplates
-  # Tuple(String, String) is the same as is being used for #send_template
-  # example: {"bookings", "booked_by_notify"}
   #
   # example implementation for multiple templates with shared fields:
   #
-  # def template_fields : Hash(Tuple(String, String), TemplateFields)
+  # def template_fields : Array(TemplateFields)
   #   common_fields = [
   #     {name: "booking_id", description: "The ID of the booking"},
   #   ]
 
-  #   {
-  #     {"bookings", "booked_by_notify"} => TemplateFields.new(
+  #   [
+  #     TemplateFields.new(
+  #       trigger: {"bookings", "booked_by_notify"},
   #       name: "Booking booked by notification",
   #       fields: common_fields
   #     ),
-  #     {"bookings", "booking_notify"} => TemplateFields.new(
+  #     TemplateFields.new(
+  #       trigger: {"bookings", "booking_notify"},
   #       name: "Booking notification",
   #       fields: common_fields + [
   #         {name: "start_time", description: "The start time of the booking"},
   #       ]
   #     ),
-  #   }
+  #   ]
   # end
   #
-  abstract def template_fields : Hash(Tuple(String, String), TemplateFields)
+  abstract def template_fields : Array(TemplateFields)
 
   alias TemplateFields = NamedTuple(
+    # The same as is being used for #send_template.
+    # example: {"bookings", "booked_by_notify"}
+    trigger: Tuple(String, String),
+
     # Human readable name
     # example: "Booking booked by notification"
     name: String,
