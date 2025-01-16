536c521a2e80d51d68a36dcc41a7220c8763af00
Stephen von Takach <steve@place.technology>
2025-01-16 15:40:32 +1100

    fix(interface/zone_access_security): improve the interface

--- src/placeos-driver/interface/zone_access_security.cr
+++ src/placeos-driver/interface/zone_access_security.cr
@@ -1,10 +1,33 @@
+require "json"
+
 abstract class PlaceOS::Driver
   module Interface::ZoneAccessSecurity
     # using an email address, lookup the security system id for a user
     abstract def card_holder_id_lookup(email : String) : String | Int64 | Nil
 
+    abstract struct CardHolderDetails
+      include JSON::Serializable
+
+      getter id : String | Int64
+      getter name : String
+      getter email : String?
+    end
+
+    # using an email address, lookup the security system id for a user
+    abstract def card_holder_lookup(id : String | Int64) : CardHolderDetails
+
     # using a name, lookup the access zone id
     abstract def zone_access_id_lookup(name : String, exact_match : Bool = true) : String | Int64 | Nil
+
+    abstract struct ZoneDetails
+      include JSON::Serializable
+
+      getter id : String | Int64
+      getter name : String
+      getter description : String?
+    end
+
+    abstract def zone_access_lookup(id : String | Int64) : ZoneDetails
 
     # return the id that represents the access permission (truthy indicates access)
     abstract def zone_access_member?(zone_id : String | Int64, card_holder_id : String | Int64) : String | Int64 | Nil
