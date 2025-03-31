ae3542e201ca4e950f72d514ff042353d25d74f0
Stephen von Takach <steve@place.technology>
2025-03-31 13:58:38 +1100

    chore(stats): provide some helper methods

--- src/placeos-driver/stats.cr
+++ src/placeos-driver/stats.cr
@@ -5,19 +5,33 @@ module PlaceOS::Driver::Stats
   # :nodoc:
   Log = ::Log.for(self)
 
+  def self.memory_usage
+    total = GC.stats.total_bytes
+    stats = GC.prof_stats
+    one_mib = 1048576
+    {
+      free:     "#{(stats.free_bytes / one_mib).round(1)}MiB",
+      heap:     "#{(stats.heap_size / one_mib).round(1)}MiB",
+      total:    "#{(total / one_mib).round(1)}MiB",
+      unmapped: "#{(stats.unmapped_bytes / one_mib).round(1)}MiB",
+      non_gc:   "#{(stats.non_gc_bytes / one_mib).round(1)}MiB",
+    }
+  end
+
+  def self.protocol_tracking
+    protocol = PlaceOS::Driver::Protocol.instance
+    {
+      tracking: protocol.@tracking.size,
+      current:  protocol.@current_requests.size,
+      next:     protocol.@next_requests.size,
+    }
+  end
+
   # Useful for debugging, it outputs memory usage and internal protocol queue values
   #
   # to obtain the stats you need to signal the process `kill -s USR2 %PID`
   def self.dump_stats
-    total = GC.stats.total_bytes
-    stats = GC.prof_stats
-    one_mib = 1048576
-    memory = "Memory Usage:\n - Free: #{(stats.free_bytes / one_mib).round(1)}MiB\n - Heap: #{(stats.heap_size / one_mib).round(1)}MiB\n - Total: #{(total / one_mib).round(1)}MiB\n - Unmapped #{(stats.unmapped_bytes / one_mib).round(1)}MiB\n - Non GC Bytes #{(stats.non_gc_bytes / one_mib).round(1)}MiB"
-
-    protocol = ::PlaceOS::Driver::Protocol.instance
-    protocol_stats = "Protocol:\n - Tracking: #{protocol.@tracking.size}\n - Current: #{protocol.@current_requests.size}\n - Next: #{protocol.@next_requests.size}"
-
-    Log.warn { "\n\n#{memory}\n#{protocol_stats}\n\n" }
+    Log.warn { "\n\n#{memory_usage}\n#{protocol_tracking}\n\n" }
   end
 
   # :nodoc:
